439e52d45eb4792c1a791a744cc9de51f5afab7a
Jesse Lee <jqlee85@gmail.com>
2019-02-09 00:19:25 -0300

    temporarily use contact link

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -14,15 +14,16 @@ class Nav extends Component {
     return <nav id="main-nav" className={theClasses}>
         
         <ul className="menu">
-          <li><Link to='/'>Home</Link></li>
+          <li><a href="https://jumpoff.io/contact/" target="_blank">Contact Us</a></li>
+          {/* <li><Link to='/'>Home</Link></li>
           <li><Link to='/about'>About</Link></li>
           <li className="login-item">
-            {/* <LoginForm 
+            <LoginForm 
               userLogin={this.props.userLogin}
               userData={this.props.userData}
               userLogout={this.props.userLogout}
-            /> */}
-          </li>
+            />
+          </li> */}
         </ul>
         
     </nav>;
